1af25313a04f77c9c953648e0c2b8321184fce78
Abraar M Nafiz <abraar@Abraars-Air.local>
2024-02-11 23:05:11 +0600

    All quants

--- Random Tasks/Breshenhams Circle Drawing Algorithm/generate_points.py
+++ Random Tasks/Breshenhams Circle Drawing Algorithm/generate_points.py
@@ -18,8 +18,28 @@ def octant_one(centre_x: int = 0, centre_y: int = 0, R:int = 10) -> list[(int, i
     return points
 
 
+def quadrant_one(centre_x: int = 0, centre_y: int = 0, R:int = 10) -> list[(int, int)]:
+
+    points_half = octant_one(centre_x, centre_y, R)
+    points_otherhalf = [(y, x) for (x, y) in points_half]
+    total_points = points_half + points_otherhalf
+    return total_points
+
+
+def draw_circle_breshenham(centre_x: int = 0, centre_y: int = 0, R:int = 10) -> list[(int, int)]:
+
+    main_quad = quadrant_one(centre_x, centre_y, R)
+    quad_2 = [(-x, y) for (x, y) in main_quad]
+    quad_3 = [(x, -y) for (x, y) in main_quad]
+    quad_4 = [(-x, -y) for (x, y) in main_quad]
+
+    total_circle_points = main_quad + quad_2 + quad_3 + quad_4
+
+    return sorted(total_circle_points)
+
+
 if __name__ == '__main__':
-    print(octant_one(0, 0, 20))
+    print(draw_circle_breshenham(0, 0, 8))
 
 
 
